4ea7673746a847dab59519acab9be57c0f3401b3
James Taylor <taylorjames9@gmail.com>
2014-04-28 13:52:44 -0500

    First commit line drawing

---
+++ LineRenderer/Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundManager : MonoBehaviour {
+
+
+		public GameObject mileMarkerPrefab; 
+
+	// Use this for initialization
+	void Start () {
+
+
+				for (int i = 0; i < 100; i ++) {
+						GameObject clone;
+						clone = Instantiate (mileMarkerPrefab, new Vector3 (i * 10, -3.5f, 0), Quaternion.identity) as GameObject;
+				}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ LineRenderer/Assets/Scripts/LineRendererScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineRendererScript : MonoBehaviour {
+
+
+		private LineRenderer lineRenderer;
+		private float counter;
+		private float dist;
+
+		public Transform origin;
+		public Transform destination;
+
+		public float lineDrawSpeed = 6.0f;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+				lineRenderer = GetComponent<LineRenderer> ();
+				lineRenderer.SetPosition (0, origin.position);
+				lineRenderer.SetWidth (0.45f, 0.45f);
+
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+		{
+				dist = Vector3.Distance (origin.position, destination.position);
+
+				if (counter < dist) {
+						counter += 0.1f / lineDrawSpeed;
+						float x = Mathf.Lerp (0, dist, counter);
+
+						Vector3 pointA = origin.position;
+						Vector3 pointB = destination.position;
+
+						//Get the unit vecotr in the desired direction, multiply by the desired lnegth and add the starting point
+						Vector3 pointAlongLine = x * Vector3.Normalize (pointB - pointA) + pointA;
+
+						lineRenderer.SetPosition (1, pointAlongLine);
+				}
+		}
+}
+	
+
